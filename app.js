babc288136f86a1958e3eac0c2c842821144762b
Jonas Dorfinger <36422131+dorfingerjonas@users.noreply.github.com>
2019-01-29 16:15:18 +0100

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const Boss = require("./boss");
-const Employee = require("./employee");
-const Person = require("./person");
-
-let persons = [new Employee('Sebastian', 'Scholl', 'CoolCompany', '3'), new Boss('Jonas', 'Dorfinger', '156', '56464654')];
