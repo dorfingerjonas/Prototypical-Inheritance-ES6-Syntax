a4228d3e751bfecd0b7c093a47ff0f3c5f358924
dorfingerjonas <jonas.dorfinger@gmx.at>
2019-02-05 17:14:35 +0100

    finished Class notation

--- student.js
+++ student.js
@@ -1,15 +1,13 @@
 const Person = require('./person');
 
-function Student (firstName, lastName, gender, hobbies, subject) {
-    Person.call(this, firstName, lastName, gender, hobbies, subject);
-    this.subject = subject;
+class Student extends Person {
+    constructor(firstName, lastName, gender, hobbies, subject) {
+        super(firstName, lastName, gender, hobbies);
+        this.subject = subject;
+    }
+    sayHello() {
+        console.log('Hi. I am ' + this.name.first + ' and I study ' + this.subject + '.');
+    }
 }
-
-Student.prototype = Object.create(Person.prototype);
-Student.prototype.constructor = Student;
-
-Student.prototype.sayHello = function () {
-    console.log('Hi. I am ' + this.name.first + ' and I study ' + this.subject + '.');
-};
 
 module.exports = Student;
--- teacher.js
+++ teacher.js
@@ -1,19 +1,17 @@
 const Person = require('./person');
 
-function Teacher(firstName, lastName, gender, hobbies, subject) {
-    Person.call(this, firstName, lastName, gender, hobbies, subject);
-    this.subject = subject;
-}
-
-Teacher.prototype = Object.create(Person.prototype);
-Teacher.prototype.constructor = Teacher;
-
-Teacher.prototype.sayHello = function () {
-    if (this.gender == 'male') {
-        console.log('Hello. My name is Mr. ' + this.name.last + ' and I teach ' + this.subject);
-    } else {
-        console.log('Hello. My name is Mrs. ' + this.name.last + ' and I teach ' + this.subject + ' .');
+class Teacher extends Person {
+    constructor(firstname, lastname, gender, hobbies, subject) {
+        super(firstname, lastname, gender, hobbies);
+        this.subject = subject;
     }
-};
+    sayHello() {
+        if (this.gender == 'male') {
+            console.log('Hello. My name is Mr. ' + this.name.last + ' and I teach ' + this.subject);
+        } else {
+            console.log('Hello. My name is Mrs. ' + this.name.last + ' and I teach ' + this.subject + ' .');
+        }
+    }
+}
 
 module.exports = Teacher;
